c32135b96ea13dd2cf4ea1f4239d6c805d786169
onlyfish <linotfish@outlook.com>
2025-11-18 19:37:35 +0800

    extra: add gpsextends and wirelessplaybackquality
    sidebar maybe work but i want A16qpr1 is release add it
    
    wirelessplaybackquality not working
    
    dsee look like not working

--- extra.mk
+++ extra.mk
@@ -20,7 +20,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # LineageOS overrides
 TARGET_EXCLUDES_AUDIOFX := true
-PRODUCT_NO_CAMERA := true
+PRODUCT_NO_CAMERA := false
 
 # Sony Framework
 ifeq ($(TARGET_SHIPS_SONY_FRAMEWORK),true)
